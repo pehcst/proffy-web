771705806ef114e779dabe4401a46d241031f8dc
Pedro Costa <pehcst@outlook.com.br>
2020-08-12 14:10:18 -0300

    added landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-
 import "./assets/styles/global.css";
+import Landing from "./pages/Landing";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <Landing />;
 }
 
 export default App;
